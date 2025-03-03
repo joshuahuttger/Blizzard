7f1db2d36bae5069f62729c027ef796a77565940
josh <josh>
2025-03-02 20:27:22 -0800

    adding path parameters

--- snowman/main.py
+++ snowman/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message":"Hello World"}
+@app.get("/user/{user_id}")
+async def get_user(user_id):
+    return {"user_id": user_id}
