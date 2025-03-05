012ea153d2ab08b20549487e5ff75579ed4808f5
josh <josh>
2025-03-04 20:21:21 -0800

    added query parameters

--- snowman/data/user.py
+++ snowman/data/user.py
@@ -1,14 +1,18 @@
 
 class User:
-    def __init__(self, name, email, credit_card_number):
-        self.__name = name
-        self.__email = email
-        self.__credit_card_number = credit_card_number
+    def __init__(self, name, email, credit_card_number, favorite_hobby):
+        self._name = name
+        self._email = email
+        self._credit_card_number = credit_card_number
+        self._favorite_hobby = favorite_hobby
     def get_name(self):
-        return self.__name
+        return self._name
 
     def get_email(self):
-        return self.__email
+        return self._email
 
     def get_credit_card_number(self):
-        return self.__credit_card_number
+        return self._credit_card_number
+
+    def favorite_hobby(self):
+        return self._favorite_hobby
--- snowman/main.py
+++ snowman/main.py
@@ -6,5 +6,9 @@ app = FastAPI()
 
 @app.get("/user/{user_id}")
 async def get_user(user_id):
-    return User("jhuggz","jhuggz@skynet.net",12343)
+    return User("jhuggz","jhuggz@skynet.net",12343, "eating chicken")
 
+@app.get("/user")
+async def get_user(name: str, email: str, credit_card_number: int, favorite_hobby: str = "netflix"):
+#http://127.0.0.1:8000/user?name=Joshua%20Huttger&email=jhuttger@skynet.net&credit_card_number=1231234&favorite_hobby=skiing
+    return User(name,email,credit_card_number, favorite_hobby)
