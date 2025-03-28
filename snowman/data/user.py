ad188d3aff0330c6cc661d38575330c42f004ffa
josh <josh>
2025-03-27 20:29:05 -0700

    pydantic model

--- snowman/data/user.py
+++ snowman/data/user.py
@@ -1,18 +1,7 @@
+from pydantic import BaseModel
 
-class User:
-    def __init__(self, name, email, credit_card_number, favorite_hobby):
-        self._name = name
-        self._email = email
-        self._credit_card_number = credit_card_number
-        self._favorite_hobby = favorite_hobby
-    def get_name(self):
-        return self._name
-
-    def get_email(self):
-        return self._email
-
-    def get_credit_card_number(self):
-        return self._credit_card_number
-
-    def favorite_hobby(self):
-        return self._favorite_hobby
+class User(BaseModel):
+    name: str
+    email: str
+    id: int
+    favorite_hobby: str | None = None
--- snowman/main.py
+++ snowman/main.py
@@ -4,11 +4,13 @@ from snowman.data.user import User
 
 app = FastAPI()
 
-@app.get("/user/{user_id}")
-async def get_user(user_id):
-    return User("jhuggz","jhuggz@skynet.net",12343, "eating chicken")
+temp_store = dict()
 
-@app.get("/user")
-async def get_user(name: str, email: str, credit_card_number: int, favorite_hobby: str = "netflix"):
-#http://127.0.0.1:8000/user?name=Joshua%20Huttger&email=jhuttger@skynet.net&credit_card_number=1231234&favorite_hobby=skiing
-    return User(name,email,credit_card_number, favorite_hobby)
+@app.get("/users/{user_id}")
+async def get_user(user_id : int):
+    return temp_store[user_id]
+
+@app.post("/users/")
+async def create_user(user: User):
+    temp_store[user.id] = user
+    return temp_store[user.id]
